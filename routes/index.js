2dc9b5584202403ffa9d1cbbd41c8c30b6211b68
Pedrin.scrk <142614058+Pedrinscrk@users.noreply.github.com>
2025-02-26 17:00:40 -0400

    add rota do products.hbs

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,14 @@ router.get("/", (req, res) => {
     res.render("home", { layout: false , title: "AquaPulse" });
 });
 
+router.get("/products", (req, res) => {
+    res.render("products", { layout: false , title: "AquaPulse - Produtos" });
+});
 
 module.exports = router;
+
+/*
+router.get("/nomeDaRota", (req, res) => {
+    res.send("nomeDoArquivo", { layout: false , title: "tituloDaPágina" });
+});
+    */
